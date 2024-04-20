2ca8742b31655823bf2a29851e262ef3c48dc1fa
Anantha Debnath <ananthadebnath103@gmail.com>
2024-04-20 18:50:32 +0600

    update to send

--- home/home.php
+++ home/home.php
@@ -15,18 +15,25 @@ mysqli_free_result($result);
 
 <h1 class="text-center pt-4z">Institutes</h1>
 
-<div class="card-deck pt-4" >
+<div class="card-deck pt-4">
     <?php
     foreach ($institutes as $institute) { ?>
-        <div class="col-md-4 pt-4" >
-            <div class="card "style='background-color:skyblue'>
+        <div class="col-md-4 pt-4">
+            <div class="card " style='background-color:skyblue'>
                 <img class="card-img-top p-2 " src="https://tribecacare.com/wp-content/uploads/2019/02/fun-activities-in-old-age-home.png" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">
                         Name: <?php echo $institute['inst_name']; ?>
                     </h5>
                     <p class="card-text">Type: <?php echo $institute['inst_type']; ?></p>
-                    <p class="card-text"><small class="text-muted"> Rating: <?php echo $institute['rating']; ?></small></p>
+                    <div class="d-flex justify-content-between">
+                        <p class="card-text"><small class="text-muted"> Rating: <?php echo $institute['rating']; ?></small></p>
+
+                        <form action="institute_info/institute_info.php" method="POST">
+                            <input type="hidden" name="id_for_info" value="<?php echo $user['id'] ?>">
+                            <button class="btn btn-success" name="info" type="submit">info</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
